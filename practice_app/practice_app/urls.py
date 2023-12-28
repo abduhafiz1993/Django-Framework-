df4352ab172c12eeb1842a1792f4d64f54189d23
abdulhafiz <muhammedabdulhafiz1993@gmail.com>
2023-12-28 09:58:11 -0800

    set up newsyear app

--- practice_app/practice_app/urls.py
+++ practice_app/practice_app/urls.py
@@ -19,5 +19,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello/', include("hello.urls"))
+    path('hello/', include("hello.urls")),
+    path('hello/', include("newyear.urls"))
 ]
