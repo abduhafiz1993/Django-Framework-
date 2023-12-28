57749e78b7aa423cb6255c046a882ac00424531d
abdulhafiz <muhammedabdulhafiz1993@gmail.com>
2023-12-28 04:21:05 -0800

    adding new route

--- practice_app/hello/urls.py
+++ practice_app/hello/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("brian/", views.brian, name="brain"),
+    path("david", views.david, name="david")
 ]
--- practice_app/hello/views.py
+++ practice_app/hello/views.py
@@ -5,3 +5,8 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("Hello World")
 
+def brian(request):
+    return HttpResponse("Hello, Brian!")
+
+def david(request):
+    return HttpResponse("Hello, David!")
