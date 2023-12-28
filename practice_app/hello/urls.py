c9195b9bd85885463ee7d023da9cf8f1907663bc
abdulhafiz <muhammedabdulhafiz1993@gmail.com>
2023-12-28 09:52:05 -0800

    update hello start up of newyear app

--- practice_app/hello/urls.py
+++ practice_app/hello/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("<str:name>", views.greet, name="greet")
+
 ]
--- practice_app/hello/views.py
+++ practice_app/hello/views.py
@@ -4,3 +4,8 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, "hello/index.html")
+
+def greet(request, name):
+    return render(request, "hello/greet.html",{
+        "name": name.capitalize()
+    })
