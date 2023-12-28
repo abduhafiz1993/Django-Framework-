bcda89185a3999fecd73bc78ed8bfb0f443945eb
abdulhafiz <muhammedabdulhafiz1993@gmail.com>
2023-12-28 10:25:31 -0800

    new year app is done

--- practice_app/practice_app/urls.py
+++ practice_app/practice_app/urls.py
@@ -20,5 +20,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('hello/', include("hello.urls")),
-    path('hello/', include("newyear.urls"))
+    path('newyear/', include("newyear.urls"))
 ]
