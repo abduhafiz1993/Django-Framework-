b8a5726de2fbc518f88d7681d3432e6ec663bc2f
abdulhafiz <muhammedabdulhafiz1993@gmail.com>
2023-12-29 11:30:58 -0800

    working with wiki route

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import Http404
 
 from . import util
 
@@ -8,3 +9,13 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def wiki(request, entry):
+    if entry not in util.list_entries():
+        raise Http404
+    content = util.get_entry(entry)
+    return render(
+        request,
+        "encyclopedia/wiki.html",
+        {"title": entry
+        },
+    )
