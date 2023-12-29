ab99cf994f8ea7c72c99acc437c4a102cb39be83
abdulhafiz <muhammedabdulhafiz1993@gmail.com>
2023-12-29 04:18:42 -0800

    update

--- practice_app/tasks/views.py
+++ practice_app/tasks/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from django import forms
 
 # Create your views here.
 tasks = ["foo", "bar", "baz"]
+
+class NewTaskForm(forms.Form):
+    task = forms.CharField(label="New Task")
 
 def index(request):
     return render(request, "tasks/index.html",{
@@ -9,4 +13,6 @@ def index(request):
     })
 
 def add(request):
-    return render(request, "tasks/add.html")
+    return render(request, "tasks/add.html", {
+        "form": NewTaskForm
+    })
