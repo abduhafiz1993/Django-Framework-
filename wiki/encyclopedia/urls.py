a6f9c4ee7b195c50581020dbcc0edd23922075b8
abdulhafiz <muhammedabdulhafiz1993@gmail.com>
2023-12-29 12:38:34 -0800

    adding path and function to new page route

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:entry>", views.wiki, name="wiki"),
-    path("search/", views.search, name="search")
+    path("search/", views.search, name="search"),
+    path("new/", views.new, name="new")
 
 ]
