b47d4949d50522f4f028608742eb8dee528d1f6c
abdulhafiz <muhammedabdulhafiz1993@gmail.com>
2023-12-29 03:27:49 -0800

    updare ttasks spp

--- practice_app/practice_app/urls.py
+++ practice_app/practice_app/urls.py
@@ -20,5 +20,7 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('hello/', include("hello.urls")),
-    path('newyear/', include("newyear.urls"))
+    path('newyear/', include("newyear.urls")),
+    path('tasks/', include("tasks.urls"))
+
 ]
---
+++ practice_app/tasks/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+tasks = ["foo", "bar", "baz"]
+
+def index(request):
+    return render(request, "tasks/index.html",{
+        "tasks": tasks
+    })
