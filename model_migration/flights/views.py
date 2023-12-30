aee891023dcada703890cc4e5aabf0e077ac9b2e
abdulhafiz <muhammedabdulhafiz1993@gmail.com>
2023-12-30 03:51:42 -0800

    correction made in some o file

--- model_migration/flights/views.py
+++ model_migration/flights/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 def index(request):
-    return render(request, "flghts/index.html",{
+    return render(request, "flights/index.html",{
         "flights": Flight.objects.all()
     })
