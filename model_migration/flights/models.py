3477d9e3c24a47df3fd0ab16cb4cac1665c10bfb
abdulhafiz <muhammedabdulhafiz1993@gmail.com>
2023-12-30 02:19:11 -0800

    adding __str__ function  to Flight class

--- model_migration/flights/models.py
+++ model_migration/flights/models.py
@@ -5,3 +5,7 @@ class Flight(models.Model):
     origin =models.CharField(max_length=64)
     destination =models.CharField(max_length=64)
     duration = models.IntegerField()
+    
+
+    def __str__(self):
+        return f"{self.id}: {self.origin} to {self.destination}"
