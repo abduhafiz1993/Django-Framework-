bf0ca25dfc4d3dae095e94a2b0bdc13ef1042429
abdulhafiz <muhammedabdulhafiz1993@gmail.com>
2023-12-30 01:59:19 -0800

    setting up flight database

--- model_migration/flights/models.py
+++ model_migration/flights/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Flight(models.Model):
+    origin =models.CharField(max_length=64)
+    destination =models.CharField(max_length=64)
+    duration = models.IntegerFeild()
+    
