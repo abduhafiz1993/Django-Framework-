61157daba40f1fcafbc397634f7bc77cce82f5c8
abdulhafiz <muhammedabdulhafiz1993@gmail.com>
2023-12-30 02:53:33 -0800

    setting up model of databse

--- model_migration/flights/models.py
+++ model_migration/flights/models.py
@@ -1,9 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Airport(models.Model):
+    code = models.CharField(max_length=3)
+    city = models.CharField(max_length=64)
+
+
+    def __str__(self):
+        return f"{self.city} ({self.code})"
+
+
 class Flight(models.Model):
-    origin =models.CharField(max_length=64)
-    destination =models.CharField(max_length=64)
+    origin =models.ForeignKey(Airport, on_delete=models.CASCADE, related_name="departures")
+    destination =models.ForeignKey(Airport, on_delete=models.CASCADE, related_name="arrivals")
     duration = models.IntegerField()
     
 
