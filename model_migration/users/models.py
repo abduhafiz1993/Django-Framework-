92ab3290408850dddb516c92c390447dd4fcc2b8
abdulhafiz <muhammedabdulhafiz1993@gmail.com>
2023-12-30 22:30:49 -0800

    update models.py

--- model_migration/users/models.py
+++ model_migration/users/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Users(models.Model):
+    username = models.CharField(max_length(64))
+    password = models.CharField(max_length(64))
