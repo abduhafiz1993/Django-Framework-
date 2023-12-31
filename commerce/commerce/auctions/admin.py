a59a76df6f8d4203373bb802a61402b0925c53f2
abdulhafiz <muhammedabdulhafiz1993@gmail.com>
2023-12-31 13:01:11 -0800

    admin,py adding file

--- commerce/commerce/auctions/admin.py
+++ commerce/commerce/auctions/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import AuctionListing, Comment, Bid
 
-# Register your models here.
+
+class AuctionListingAdmin(admin.ModelAdmin):
+    list_display = ('title', 'current_price', 'end_time', 'seller', 'closed')
+    list_filter = ('category', 'closed')
+    search_fields = ('title', 'description', 'seller__username')
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('commenter', 'auction_listing', 'comment_time')
+    list_filter = ('auction_listing__category',)
+    search_fields = ('text', 'commenter__username')
+
+class BidAdmin(admin.ModelAdmin):
+    list_display = ('bidder', 'auction_listing', 'amount', 'bid_time')
+    list_filter = ('auction_listing__category',)
+    search_fields = ('bidder__username', 'auction_listing__title')
+
+
+admin.site.register(AuctionListing, AuctionListingAdmin)
+admin.site.register(Comment, CommentAdmin)
+admin.site.register(Bid BidAdmin)
