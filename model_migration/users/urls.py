72c388989eb4888fb9f1c44e17524078aef91817
abdulhafiz <muhammedabdulhafiz1993@gmail.com>
2023-12-31 04:59:52 -0800

    adding user autentication

--- model_migration/users/urls.py
+++ model_migration/users/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name= "index")
+    path('', views.index, name="index"),
+    path("login", views.login_view, name="login"),
+    path("logout", views.logout_view, name="logout")
 ]
--- model_migration/users/views.py
+++ model_migration/users/views.py
@@ -1,5 +1,35 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+
 
 # Create your views here.
 def index(request):
-    return render(request, "users/index.html")
+    # If no user is signed in, return to login page:
+    if not request.user.is_authenticated:
+        return HttpResponseRedirect(reverse("login"))
+    return render(request, "users/user.html")
+
+
+def login_view(request):
+    if request.method == "POST":
+        # Accessing username and password from form data
+        username = request.POST["username"]
+        password = request.POST["password"]
+
+        # Check if username and password are correct, returning User object if so
+        user = authenticate(request, username=username, password=password)
+
+        # If user object is returned, log in and route to index page:
+        if user:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        # Otherwise, return login page again with new context
+        else:
+            return render(request, "users/login.html", {
+                "message": "Invalid Credentials"
+            })
+     return render(request, "users/login.html")
+
+def logout_view(request):
+    # Pass is a simple way to tell python to do nothing.
+    
