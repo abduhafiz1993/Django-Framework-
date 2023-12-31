6d85e20adf8e2580bb4e5f5b3a71a675f41d6c5d
abdulhafiz <muhammedabdulhafiz1993@gmail.com>
2023-12-31 00:15:57 -0800

    update in flights route

--- model_migration/flights/urls.py
+++ model_migration/flights/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
 
+app_name = "flights"
+
 urlpatterns = [
     path('', views.index, name="index"),
     path('flight/<int:number>', views.flight, name="flight"),
-    
+
 ]
