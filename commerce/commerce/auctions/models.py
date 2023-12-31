6a4aba93655b5c73f73c85ab8283bda2525488cf
abdulhafiz <muhammedabdulhafiz1993@gmail.com>
2023-12-31 12:54:31 -0800

    adding modls for thhe project

--- commerce/commerce/auctions/models.py
+++ commerce/commerce/auctions/models.py
@@ -4,3 +4,42 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+
+class AuctionListing(models.Model):
+    
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    starting_price = models.DecimalField(max_digits=10, decimal_places=2)
+    current_price = models.DecimalField(max_digits=10, decimal_places=2)
+    end_time = models.DateTimeField()
+    seller = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='auction_images/', blank=True, null=True)
+    category = models.CharField(max_length=50, blank=True, null=True)
+    starting_bid = models.DecimalField(max_digits=10, decimal_places=2)
+    winner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='won_auctions', null=True, blank=True)
+    closed = models.BooleanField(default=False)
+
+
+    def __str__(self):
+        return f"Auction id: {self.id}, title: {self.title} Description:{self.description} Starting_price: {self.starting_price} Current_price:{self.current_price} End_time:{self.end_time} Seller:{self.seller} Image:{self.image}  Category:{self.category} Starting_bid: {self.starting_bid} winner: {self.winner} closed:{self.closed}" 
+
+
+class Bid(models.Model):
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    bid_time = models.DateTimeField(auto_now_add=True)
+    bidder = models.ForeignKey(User, on_delete=models.CASCADE)
+    auction_listing = models.ForeignKey(AuctionListing, on_delete=models.CASCADE)
+    def __str__(self):
+        return f"Amount:{self.amount} Bid_time:{self.bid_time} Bidder: {self.bidder} Acution_listing:{self.auction_listing}"
+
+
+
+class Comment(models.Model):
+    text = models.TextField()
+    comment_time = models.DateTimeField(auto_now_add=True)
+    commenter = models.ForeignKey(User, on_delete=models.CASCADE)
+    auction_listing = models.ForeignKey(AuctionListing, on_delete=models.CASCADE)
+    def __str__(self):
+        return f"Text: {self.text} comment_time: {self.comment_time} commenter:{self.commenter} auction_listing: {self.auction_listing}"
