342cc16810db997b39d83ce81b0071c409a458dd
abdulhafiz <muhammedabdulhafiz1993@gmail.com>
2023-12-31 22:51:35 -0800

    upadte on CL route

--- commerce/commerce/auctions/urls.py
+++ commerce/commerce/auctions/urls.py
@@ -2,9 +2,12 @@ from django.urls import path
 
 from . import views
 
+app_name = "auction"
+
 urlpatterns = [
     path("", views.index, name="index"),
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
-    path("register", views.register, name="register")
+    path("register", views.register, name="register"),
+    path("create_listing", views.create_listing, name="createListing")
 ]
