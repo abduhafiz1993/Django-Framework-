3d8934463ad7f9553b4d6a372d5b2b4b4f5f4f9a
abdulhafiz <muhammedabdulhafiz1993@gmail.com>
2024-01-08 13:49:17 -0800

    adding event listtner to compose-view

--- mail/mail/static/mail/inbox.js
+++ mail/mail/static/mail/inbox.js
@@ -1,8 +1,3 @@
-emailsView = document.querySelector('#emails-view');
-compose = document.querySelector('#compose-view');
-recipients = document.querySelector('#compose-recipients');
-subject =  document.querySelector('#compose-subject');
-body = document.querySelector('#compose-body');
 
 
 function compose_email() {
@@ -25,9 +20,23 @@ function load_mailbox(mailbox) {
 
   // Show the mailbox name
   emailsView.innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+  console.log("am working load")
+}
+
+
+function send_email(){
+  console.log("hi")
+  return false
 }
 
 document.addEventListener('DOMContentLoaded', function() {
+
+  // varibales
+  emailsView = document.querySelector('#emails-view');
+  compose = document.querySelector('#compose-view');
+  recipients = document.querySelector('#compose-recipients');
+  subject =  document.querySelector('#compose-subject');
+  body = document.querySelector('#compose-body');
 
   // Use buttons to toggle between views
   document.querySelector('#inbox').addEventListener('click', () => load_mailbox('inbox'));
@@ -35,8 +44,10 @@ document.addEventListener('DOMContentLoaded', function() {
   document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
   document.querySelector('#compose').addEventListener('click', compose_email);
 
+  document.querySelector('#compose-form').onsubmit = send_email;
   // By default, load the inbox
   load_mailbox('inbox');
 
-  
+
 });
+
